709f5399be746b2bc0e25666db9086e32a560c9e
keshavrajcg <keshavraj.cg@gmail.com>
2026-08-23 22:44:08 +0530

    variable file added

---
+++ python-02/variable.py
@@ -0,0 +1,36 @@
+#Q24
+name = "Keshav"
+age = "19"
+city = "Samastipur"
+print(name,age,city)
+
+#25
+student_name = "Ronal"
+student_roll_number = "68"
+student_branch = "Cse"
+
+#26
+marks = "95"
+marks = "100"
+
+#27
+name = "Rahul"
+age = 18
+city = "Patna"
+city = "Delhi"
+age = 20
+
+#28
+x = y = z = 0
+
+#29
+student_1 = "Rahul"
+student_name = "Rahul"
+class_ =  "B.Tech"
+
+#30
+student_name = "Divesh"
+student_age = 18
+#after 1 year student's age has been reassigned
+student_age = 19
+print(student_name,student_age)
