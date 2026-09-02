255f2255917e93af2dbd648e547a56fd4088d0d6
keshavrajcg <keshavraj.cg@gmail.com>
2026-09-03 01:10:29 +0530

    added file bool

---
+++ python-06/booleanop.py
@@ -0,0 +1,36 @@
+#Q25
+number = 25
+print(number > 10 and number < 50)
+
+#Q26
+number = 25
+print(number < 10 or number > 100)
+
+#Q27
+number = 25
+print(not number > 10)
+
+#Q28
+print(bool(0)) #it is a false value
+print(bool(1)) #it is a true value
+print(bool(-5)) #it is a true value
+print(bool("")) #it is a false value
+print(bool("Python")) #it is a true value
+print(bool(False)) #it is a false value
+print(bool(True)) #it is a true value
+print(bool(None)) #it is a false value
+
+#Q29
+print(bool(0))
+print(bool(10))
+print(bool(""))
+print(bool("Hello"))
+print(bool(None))
+
+#Q30
+print(0, type(0), bool(0))
+print(1, type(1), bool(1))
+print("", type(""), bool(""))
+print("Python", type("Python"), bool("Python"))
+print(False, type(False), bool(False))
+print(None, type(None), bool(None))
